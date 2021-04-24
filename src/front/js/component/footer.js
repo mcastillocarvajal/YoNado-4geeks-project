6fccaa8f6fe00d6bbe4e43197eff366f212d36d2
alonsog3a <alonsog3a@gmail.com>
2021-04-24 01:59:07 +0000

    Modificacion Footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,60 +1,63 @@
 import React, { Component } from "react";
+import "../../styles/footer.scss";
+import cindeImageUrl from "../../img/cinde-logo.png";
 
 export const Footer = () => (
 	<footer className="dk-footer" id="dk-footer">
-		<div className="container">
+		<div className="container-fluid">
 			<div className="row">
-				<div className="col-md-12 col-lg-4">
+				<div className="col-md-3 ">
 					<div className="dk-footer-box-info">
 						<a className="footer-logo">
-							<img
+							<h3>YO NADO</h3>
+							{/* <img
 								src="https://i.pinimg.com/originals/f4/e1/a8/f4e1a873f388d9e75a53bf7d84349963.png"
 								className="img-fluid"
-							/>
+							/> */}
 						</a>
+					</div>
+				</div>
+				<div className="col-3">
+					<div className="dk-footer-box-info">
 						<p className="footer-info-text">
 							Este página, fue realizada con fines educativos. Como referencia para nuestro proyecto
 							final. Agradecemos a todas las empresas que estuvieron involucradas para que esto pueda ser
 							realidad.
 						</p>
-					</div>
-					<div className="footer-social-link">
-						<h3>Sigue</h3>
-						<ul>
-							<li>
-								<a href="https://4geeksacademy.com/es/inicio">
-									<img
-										className="fa fa-4geeks"
-										src="https://uploads.convertflow.co/production/websites/9849/8uSlRHnaTm6nHOBiBNxU_6FXhIqn9QMS7umwEj84p_4G_LOGO_NEGRO-01.png"
-										width="75px"
-										height="50px"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="https://www.micit.go.cr/">
-									<img
-										className="fa fa-4geeks"
-										src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Micitt_logo.png"
-										width="80px"
-										height="50px"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="https://www.micit.go.cr/becas/programa-innovacion-y-capital-humano-la-competitividad-pinn">
-									<img
-										className="fa fa-4geeks"
-										src="http://vinv.ucr.ac.cr/sites/default/files/capsulas/pinn_2.jpg"
-										width="110px"
-										height="50px"
-									/>
-								</a>
-							</li>
-						</ul>
+						<div className="footer-social-link">
+							<h3>Más Información</h3>
+							<ul>
+								<li>
+									<a href="https://4geeksacademy.com/es/inicio">
+										<img
+											className="fa fa-4geeks"
+											src="https://uploads.convertflow.co/production/websites/9849/8uSlRHnaTm6nHOBiBNxU_6FXhIqn9QMS7umwEj84p_4G_LOGO_NEGRO-01.png"
+											width="80px"
+											height="50px"
+										/>
+									</a>
+								</li>
+								<li>
+									<a href="https://www.micit.go.cr/">
+										<img
+											className="fa fa-4geeks"
+											src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Micitt_logo.png"
+											width="80px"
+											height="50px"
+										/>
+									</a>
+								</li>
+								<li>
+									<a href="https://www.cinde.org/es">
+										<img className="fa fa-4geeks" src={cindeImageUrl} width="100px" height="80px" />
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-				<div className="col-md-12 col-lg-8">
+
+				<div className="col-3">
 					<div className="row">
 						<div className="col-md-12 col-lg-6">
 							<div className="footer-widget footer-left-widget">
@@ -76,7 +79,7 @@ export const Footer = () => (
 								</ul>
 							</div>
 						</div>
-						<div className="col-md-12 col-lg-6">
+						<div className="col-3">
 							<div className="footer-widget footer-left-widget">
 								<ul>
 									<h3>Registro</h3>
@@ -92,35 +95,12 @@ export const Footer = () => (
 					</div>
 				</div>
 			</div>
-		</div>
 
-		{/* <div className="copyright">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<span>Copyright © 2021, All Right Reserved</span>
-					</div>
-
-					<div className="col-md-6">
-						<div className="copyright-menu">
-							<ul>
-								<li>
-									<a href="#">Home</a>
-								</li>
-								<li>
-									<a href="#">Terms</a>
-								</li>
-								<li>
-									<a href="#">Privacy Policy</a>
-								</li>
-								<li>
-									<a href="#">Contact</a>
-								</li>
-							</ul>
-						</div>
-					</div>
+			<div className="row">
+				<div className="col">
+					<span className="copyright">Copyright © 2021, All Right Reserved</span>
 				</div>
 			</div>
-		</div> */}
+		</div>
 	</footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
-import { Icons } from "../component/Icons"
+import { Icons } from "../component/Icons";
 import { Register } from "./register";
 import { Perfil } from "./perfil";
 import { Landing } from "../component/landing";
