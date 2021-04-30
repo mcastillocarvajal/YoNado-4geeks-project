44bfa27083abefe127cbc237d22b8518a19af261
Moises Castillo <mcastillo093@gmail.com>
2021-04-30 21:21:01 +0000

    endpoints to reset password are completed

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,67 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Favorite, Activity
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 api = Blueprint('api', __name__)
+
+
+# >>>>>>>> RESET PASSWORD
+
+
+@api.route('/resetpassword', methods=['POST'])
+def handle_resetpassword():
+
+    email = request.json.get("email", None)
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"msg": "Email not found"}), 401
+
+    access_token = create_access_token(identity=email)
+    link = access_token[249:]
+
+    message = Mail(
+        from_email='yonado4geeks@gmail.com',
+        to_emails=email,
+        subject='Restablecer contraseña',
+        html_content=
+        f'<h3>Hola {user.name},<h3><br></br><p>Recibimos tu solicitud para restablecer tu acceso en Yo Nado.</p><br></br><p>Accede a este link para cambiar tu contraseña: <a href="https://3000-indigo-bison-5u2nk30k.ws-us04.gitpod.io/reset/{link}">Click aquí!</a></p>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        data = {
+            "user": user.serialize(),
+            "link": link,
+        }
+        return jsonify(data)
+
+    except Exception as e:
+        print(e.message)
+
+
+@api.route('/password', methods=['PUT'])
+def update_password():
+    
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    
+    user.password = password	
+    db.session.commit()
+
+    response = {
+	"msg": "Password updated sucessfully"
+    }  	
+    return jsonify(response), 200
 
 
 # >>>>>>>  LOGIN / REGISTER ENDPOINT
